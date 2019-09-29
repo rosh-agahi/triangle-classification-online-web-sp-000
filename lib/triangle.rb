397787e2e66221f4e4d2abfadca1c302599a46a4
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-29 19:55:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,16 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :side_one, :side_two, :side_three
+  
+  def initialize(sides)
+    sides.each {|side,size| self.send(("#{side}="),size)
+  end
+  
+  def kind 
+    if @side_one == @side_two == @side_three
+      :equilateral
+    elsif @side_one == @side_two != @side_three || @side_two == @side_three != @side_one
+      :isosceles
+    elsif 
+
 end
