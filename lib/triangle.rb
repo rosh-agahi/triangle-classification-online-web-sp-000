0ac5905dc55d7f5a57e2477a6f6049845d448ad8
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-29 20:05:21 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :side_one, :side_two, :side_three
+  
+  def initialize(side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
+  end
+  
+  def kind
+    if (side_one <= 0 || side_two <= 0 || side_three <= 0) || (side_one >= side_two + side_three || side_two >= side_one + side_three || side_three >= side_one + side_two)
+      raise TriangleError
+    elsif side_one == side_two && side_one == side_three
+      :equilateral
+    elsif (side_one == side_two && side_two != side_three) || (side_one == side_three && side_one != side_two) || (side_two == side_three && side_one != side_three) 
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  
+  class TriangleError < StandardError
+    def message
+      "No values less than or equal to 0 are valid for side length. The length of one side cannot be greater than the sum of the other two sides."
+    end
+  end
 end
